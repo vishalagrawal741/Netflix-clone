1bf60fdf853859915d9540cddc9e75b2b2b486ce
Vishal Agrawal <vishalagrawal7489@gmail.com>
2025-08-22 23:31:27 +0530

    add scroll in cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data.js'
 
+
+
 const TitleCards = () => {
+
+  const cardsRef = useRef();
+
+const handleWheel= (event)=>{
+  event.preventDefault;
+  cardsRef.current.scrollLeft +=event.deltaY;
+}
+
+useEffect(()=>{
+  cardsRef.current.addEventListener('wheel', handleWheel);
+},[])
+
   return (
-    <div className='titleCards'>
+    <div className='title-cards'>
       <h2>Popular on Netflix</h2>
-      <div className="card-list">
+      <div className="card-list" ref={cardsRef}>
         {
           cards_data.map((card, index) => {
             return (
